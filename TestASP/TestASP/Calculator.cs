7c397cda2807357755168f3bc2bc5a89f2cba776
Meny22 <meny.metekia@gmail.com>
2016-03-16 11:43:11 +0100

    Unit test

---
+++ TestASP/TestASP/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestASP
+{
+    public class Calculator
+    {
+        public int Plus(int first, int second)
+        {
+            return first + second;
+        }
+
+        public int Min(int first, int second)
+        {
+            return first - second;
+        }
+
+        public int Keer(int first, int second)
+        {
+            return first * second;
+        }
+    }
+}
---
+++ TestASP/UnitTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestASP;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Calculator calculator = new Calculator();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(4, calculator.Plus(2, 2));
+            Assert.AreEqual(7, calculator.Plus(5, 2));
+            Assert.AreEqual(1, calculator.Min(100, 99));
+            Assert.AreEqual(55, calculator.Min(100, 45));
+            Assert.AreEqual(20, calculator.Keer(5, 4));
+            Assert.AreEqual(100, calculator.Keer(100, 1));
+        }
+    }
+}
